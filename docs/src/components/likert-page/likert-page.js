8be631874e6abff00e9825bbe3289014e8ee1a17
Wen Zhang <wz2705@gmail.com>
2022-12-08 19:47:27 -0500

    added likert scale

--- docs/src/components/likert-page/likert-page.js
+++ docs/src/components/likert-page/likert-page.js
@@ -12,11 +12,11 @@ const LikertPage = (props) => {
     }
 
     return (
-        <div>
+        <div className="w-100">
             <h1>For each of the following statements, rate the extent to which you 
                 <span className="fw-bold fst-italic"> agree/disagree:</span>
             </h1>
-            <div className="text-left bg-red">
+            <div className="text-left w-30 mt-5">
                 <LikertScale msg="I can tell the difference between artist pieces and AI-generated content." />
                 <LikertScale msg="AI-generated content and original artist pieces should be given equal value." />
                 <LikertScale msg="I prefer original artist pieces over AI-generated content." />
--- docs/src/components/likert-page/likert-scale/likert-scale.js
+++ docs/src/components/likert-page/likert-scale/likert-scale.js
@@ -1,13 +1,31 @@
+import "./likert-scale.css";
 import React from "react";
+import Likert from 'react-likert-scale';
 
 const LikertScale = (props) => {
-
-    return (
-        <div className="bg-red p-2">
-            <p className="text-left">{props.msg}</p>
+  const likertOptions = {
+    question: null,
+    responses: [
+      { value: 1, text: "Strongly Disagree" },
+      { value: 2, text: "Disagree" },
+      { value: 3, text: "Neutral", checked: true },
+      { value: 4, text: "Agree" },
+      { value: 5, text: "Strongly Agree" }
+    ],
+    onChange: val => {
+      console.log(val);
+    }
+  };
+  return (
+    <div className="d-flex">
+        <div className="w-50 mw-50">
+            <p className="d-block mw-50 float-left">{props.msg}</p>
         </div>
-    );
-
+        <div className="w-50">
+            <Likert {...likertOptions} />
+        </div>
+    </div>
+  )
 }
 
 export default LikertScale;
